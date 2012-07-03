787207f19fcc433ac10a3857e4fed7ea8dbd5dca
Heinrich Klobuczek <heinrich@mail.com>
2012-07-03 11:57:27 +0530

    renamed columns to be less confusing with collection names

--- sample/script.rb
+++ sample/script.rb
@@ -1,8 +1,8 @@
 Transform::Dsl.draw do
-  define_collection :a, :a, :b, :c
-  calculate(:b, :a, :sum) { |a| a.a.to_i + a.b.to_i + a.c.to_i }
+  define_collection :a, :c1, :c2, :c3
+  calculate(:b, :a, :sum) { |a| a.c1.to_i + a.c2.to_i + a.c3.to_i }
   compose(:c, :a, :b)
-  slice :d, :c, :a, :sum
+  slice :d, :c, :c1, :sum
   filter(:e, :d) {|d| d.sum.to_i > 6}
   aggregate(:f, :e, 0, :total) {|total, e| total + e.sum.to_i}
   store :f
